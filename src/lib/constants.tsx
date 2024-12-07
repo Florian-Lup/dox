4f7b555f5ff91408e6699de9dd0b552043dff276
Florian-Lup <98991486+Florian-Lup@users.noreply.github.com>
2024-12-07 02:22:28 +0000

    added tones

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -29,3 +29,13 @@ export const userNames = [
 export const userColors = ['#fb7185', '#fdba74', '#d9f99d', '#a7f3d0', '#a5f3fc', '#a5b4fc', '#f0abfc']
 
 export const themeColors = ['#fb7185', '#fdba74', '#d9f99d', '#a7f3d0', '#a5f3fc', '#a5b4fc']
+
+export const tones = [
+  { value: 'professional', label: 'Professional' },
+  { value: 'casual', label: 'Casual' },
+  { value: 'friendly', label: 'Friendly' },
+  { value: 'formal', label: 'Formal' },
+  { value: 'enthusiastic', label: 'Enthusiastic' },
+  { value: 'confident', label: 'Confident' },
+  { value: 'direct', label: 'Direct' },
+]
