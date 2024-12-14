85859ec9bc4d3c5f2e6b444f2cdc5205aa2e1c4d
Florian-Lup <98991486+Florian-Lup@users.noreply.github.com>
2024-12-14 15:19:09 +0000

    comment fix

--- src/components/menus/ContentItemMenu/ContentItemMenu.tsx
+++ src/components/menus/ContentItemMenu/ContentItemMenu.tsx
@@ -49,49 +49,26 @@ export const ContentItemMenu = ({ editor }: ContentItemMenuProps) => {
           </Popover.Trigger>
           <Popover.Content side="bottom" align="start" sideOffset={8}>
             <Surface className="p-2 flex flex-col min-w-[16rem]">
-              <Popover.Close>
+              <Popover.Close asChild>
                 <DropdownButton onClick={actions.resetTextFormatting}>
                   <Icon name="RemoveFormatting" />
                   Clear formatting
                 </DropdownButton>
               </Popover.Close>
-              <Popover.Close>
+              <Popover.Close asChild>
                 <DropdownButton onClick={actions.copyNodeToClipboard}>
                   <Icon name="Clipboard" />
                   Copy to clipboard
                 </DropdownButton>
               </Popover.Close>
-              <Popover.Close>
+              <Popover.Close asChild>
                 <DropdownButton onClick={actions.duplicateNode}>
                   <Icon name="Copy" />
                   Duplicate
                 </DropdownButton>
               </Popover.Close>
-              <Popover.Root>
-                <Popover.Trigger asChild>
-                  <DropdownButton>
-                    <Icon name="MessageSquareText" />
-                    Comment
-                  </DropdownButton>
-                </Popover.Trigger>
-                <Popover.Portal>
-                  <Popover.Content side="right" sideOffset={8} asChild>
-                    <Surface className="p-3 flex flex-col gap-2">
-                      <div className="flex flex-col gap-1">
-                        <textarea
-                          placeholder="Add a comment..."
-                          className="w-[280px] h-24 px-2 py-1.5 text-sm rounded border border-neutral-200 dark:border-neutral-700 bg-transparent focus:outline-none focus:ring-1 focus:ring-neutral-400 dark:focus:ring-neutral-500 resize-none"
-                        />
-                        <button className="self-end px-3 py-1.5 text-sm rounded-md bg-neutral-100 hover:bg-neutral-200 dark:bg-neutral-800 dark:hover:bg-neutral-700 transition-colors">
-                          Send
-                        </button>
-                      </div>
-                    </Surface>
-                  </Popover.Content>
-                </Popover.Portal>
-              </Popover.Root>
               <Toolbar.Divider horizontal />
-              <Popover.Close>
+              <Popover.Close asChild>
                 <DropdownButton
                   onClick={actions.deleteNode}
                   className="text-red-500 bg-red-500 dark:text-red-500 hover:bg-red-500 dark:hover:text-red-500 dark:hover:bg-red-500 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-20"
